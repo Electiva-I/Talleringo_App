7f0107ac0d13ab502708c6a4191501147246bc68
JhosepIslam <jhosepchai@gmail.com>
2021-03-30 20:42:01 -0600

    added views and configurations

---
+++ app/src/main/java/com/islam/talleringo/utils/utils.java
@@ -0,0 +1,28 @@
+package com.islam.talleringo.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.islam.talleringo.activities.LoginActivity;
+import com.islam.talleringo.activities.MainActivity;
+
+public class utils {
+    public static final int GOOGLE_SIGN_IN_FLAG = 901;
+
+    public static Intent  updateUI(FirebaseUser user, Context context){
+        Intent intent;
+        if (user != null){
+            intent = new Intent(context, MainActivity.class);
+        }else {
+            intent = new Intent(context, LoginActivity.class);
+        }
+        return  intent;
+    }
+
+    public static void signOut(){
+        FirebaseAuth.getInstance().signOut();
+    }
+}
