c49379da3c18ead7449ad5ba890c822a0431ba38
Platero Julio <julioplatero98@gmail.com>
2021-04-20 19:03:18 -0600

    Update FragmentSettings and AddMaintenanceDialog

--- app/src/main/java/com/islam/talleringo/dialogs/AddMaintenanceDialog.java
+++ app/src/main/java/com/islam/talleringo/dialogs/AddMaintenanceDialog.java
@@ -2,12 +2,15 @@ package com.islam.talleringo.dialogs;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,9 +22,11 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -35,12 +40,16 @@ import com.islam.talleringo.utils.utils;
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.util.Calendar;
 
 public class AddMaintenanceDialog extends DialogFragment {
 
     private Button btn_add, btn_cancel;
     private Spinner cboxVehicle;
-    private EditText vehicle, detalle, costo, fecha;
+    private EditText vehicle, detalle, costo;
+    private TextView fecha;
+    int year, month, day;
+    DatePickerDialog.OnDateSetListener setListener;
 
     String[] Vehicles = {"Seleccione","Mazda RX","Toyota Tacoma","Nissan GTR"};
 
@@ -54,8 +63,41 @@ public class AddMaintenanceDialog extends DialogFragment {
         cboxVehicle = view.findViewById(R.id.vehicle_text);
         costo = view.findViewById(R.id.cost_text);
         costo.setEnabled(false);
+        fecha = view.findViewById(R.id.date_text);
         cboxVehicle.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, Vehicles));
+        CallDateDialog();
         return view;
+    }
+
+    public void CallDateDialog(){
+        Calendar calendar = Calendar.getInstance();
+         year = calendar.get(Calendar.YEAR);
+         month = calendar.get(Calendar.MONTH);
+         day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                month = m+1;
+                String date = day+"/"+month+"/"+year;
+                fecha.setText(date);
+            }
+        };
+        fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext(),
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        setListener,
+                        year,
+                        month,
+                        day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+
     }
 
     @NonNull
